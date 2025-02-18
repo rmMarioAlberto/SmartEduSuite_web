ba9cd9afd1b3cca9d86e6871df843ce25d94a383
rmMarioAlberto <2023371044@uteq.edu.mx>
2025-02-18 09:50:17 -0600

    Cambios url logi

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 export const login = async (correo, contra) => {
     
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch('https://smar-edu-suite-backend.vercel.app/web/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -22,7 +22,7 @@ export const login = async (correo, contra) => {
 };
 
 export const changePassword = async (newPassword, id) => {
-    const response = await fetch('http://localhost:5000/api/auth/change-password', {
+    const response = await fetch('https://smar-edu-suite-backend.vercel.app/web/change-password', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
